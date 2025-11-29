5375921808f7c616fb0eb2abcba46d0c03947e98
Daggi Developer <daggi@example.com>
2025-11-29 22:50:00 +0300

    sb

--- program/src/components/ui/sidebar.jsx
+++ program/src/components/ui/sidebar.jsx
@@ -170,9 +170,10 @@ const Sidebar = forwardRef(
                     <SheetContent
                         data-sidebar="sidebar"
                         data-mobile="true"
-                        className="w-[--sidebar-width] bg-sidebar p-0 text-sidebar-foreground [&>button]:hidden"
+                        className="w-[--sidebar-width] p-0 text-sidebar-foreground [&>button]:hidden"
                         style={{
                             "--sidebar-width": SIDEBAR_WIDTH_MOBILE,
+                            backgroundColor: "#ffffff",
                         }}
                         side={side}
                     >
